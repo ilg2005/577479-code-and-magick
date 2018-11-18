b58efd91fbd3cd559861a78f1a31ecae6967c4de
ilg2005 <igoldfarb10@gmail.com>
2018-11-18 14:53:30 +0300

    добавлены координаты облака и функция для его отрисовки

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,15 @@
 'use strict';
 
-var renderStatistics = function (ctx, names, times) {
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_HEIGHT = 270;
+var CLOUD_WIDTH = 420;
 
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)');
 };
