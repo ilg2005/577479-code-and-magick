e61080d4b2199e1fb8a98ba7a628e6c6ab8b40ca
ilg2005 <igoldfarb10@gmail.com>
2018-11-18 16:38:29 +0300

    добавлена подпись имени игрока на диаграмму

--- js/stat.js
+++ js/stat.js
@@ -21,18 +21,18 @@ var renderCloudText = function (ctx, msg, x, y, align) {
   ctx.fillText(msg, x, y);
 };
 
-var renderStatsColumn = function (ctx, i, times) {
+var renderStatsColumn = function (ctx, i, players, times) {
   var maxTime = Math.max.apply(null, times);
   var barHeight = BAR_MAX_HEIGHT * times[i] / maxTime;
   ctx.fillRect(CLOUD_X + BAR_SPACE + (BAR_WIDTH + BAR_SPACE) * i, CLOUD_HEIGHT - 3 * SPACE - barHeight, BAR_WIDTH, barHeight);
 
-
+  renderCloudText(ctx, players[i], CLOUD_X + BAR_SPACE + (BAR_WIDTH + BAR_SPACE) * i, CLOUD_HEIGHT - SPACE);
 };
 
-var renderStatistics = function (ctx, names, times) {
+var renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + SPACE, CLOUD_Y + SPACE, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)');
   renderCloudText(ctx, 'Ура вы победили!', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + 2 * SPACE, 'center');
   renderCloudText(ctx, 'Список результатов:', CLOUD_X + SPACE, CLOUD_Y + 4 * SPACE, 'left');
-  renderStatsColumn(ctx, 1, times);
+  renderStatsColumn(ctx, 1, players, times);
 };
