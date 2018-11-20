ea047997da92df257a233490eb8918d7b0a09a5b
ilg2005 <igoldfarb10@gmail.com>
2018-11-20 22:26:20 +0300

    добавлены массивы имен и фамилий

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
 var setup = document.querySelector('.overlay');
 
 setup.classList.remove('hidden');
