eb5a13e4e8c7258cba136ab6d04ca5067ccd775f
ilg2005 <igoldfarb10@gmail.com>
2018-11-20 22:32:18 +0300

    добавлен массив EYES_COLORS

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,7 @@
 var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var setup = document.querySelector('.overlay');
 
