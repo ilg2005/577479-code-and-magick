1b36d7c8e9780082ff1ade38caf07193269b3338
ilg2005 <igoldfarb10@gmail.com>
2018-12-09 13:16:27 +0300

    реализовано перемещение окна диалога

--- js/dialog.js
+++ js/dialog.js
@@ -3,9 +3,33 @@
 var setupElement = document.querySelector('.setup');
 var userPicElement = setupElement.querySelector('.setup-user-pic');
 
-var userPicElementMouseDownHandler = function (evt) {
 
-  console.log('Нажатие ЛКМ на userPic');
+var userPicElementMouseDownHandler = function (evtMouseDown) {
+  evtMouseDown.preventDefault();
+  var startMouseCoordinates = {
+    x: evtMouseDown.clientX,
+    y: evtMouseDown.clientY
+  };
+  var dragged = false;
+
+  var documentMouseMoveHandler = function (evtMouseMove) {
+    dragged = true;
+    var shift = {
+      x: evtMouseMove.clientX - startMouseCoordinates.x,
+      y: evtMouseMove.clientY - startMouseCoordinates.y
+    };
+
+    startMouseCoordinates = {
+      x: evtMouseMove.clientX,
+      y: evtMouseMove.clientY
+    };
+
+    setupElement.style.left = (setupElement.offsetLeft + shift.x) + 'px';
+    setupElement.style.top = (setupElement.offsetTop + shift.y) + 'px';
+  };
+
+  document.addEventListener('mousemove', documentMouseMoveHandler);
+  document.addEventListener('mouseup', documentMouseupHandler);
 };
 
 userPicElement.addEventListener('mousedown', userPicElementMouseDownHandler);
