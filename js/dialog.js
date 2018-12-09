f9ab701d0d56de293770c49a7af7300344e20a1a
ilg2005 <igoldfarb10@gmail.com>
2018-12-09 12:56:53 +0300

    добавлен z-index на .setup-user-pic

--- js/dialog.js
+++ js/dialog.js
@@ -2,17 +2,11 @@
 
 var setupElement = document.querySelector('.setup');
 var userPicElement = setupElement.querySelector('.setup-user-pic');
-var uploadElement = setupElement.querySelector('.upload');
-
-var uploadElementMouseDownHandler = function (evt) {
-  evt.preventDefault();
-  console.log('Нажатие ЛКМ на upload');
-};
 
 var userPicElementMouseDownHandler = function (evt) {
-  evt.preventDefault();
+
   console.log('Нажатие ЛКМ на userPic');
 };
 
 userPicElement.addEventListener('mousedown', userPicElementMouseDownHandler);
-uploadElement.addEventListener('mousedown', uploadElementMouseDownHandler);
+
