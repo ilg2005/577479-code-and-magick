9e79c6faa9145c533ffe625eac7669df40f2401f
ilg2005 <igoldfarb10@gmail.com>
2018-12-09 12:14:54 +0300

    поставлен обработчик mousedown

--- js/dialog.js
+++ js/dialog.js
@@ -1,2 +1,18 @@
 'use strict';
 
+var setupElement = document.querySelector('.setup');
+var userPicElement = setupElement.querySelector('.setup-user-pic');
+var uploadElement = setupElement.querySelector('.upload');
+
+var uploadElementMouseDownHandler = function (evt) {
+  evt.preventDefault();
+  console.log('Нажатие ЛКМ на upload');
+};
+
+var userPicElementMouseDownHandler = function (evt) {
+  evt.preventDefault();
+  console.log('Нажатие ЛКМ на userPic');
+};
+
+userPicElement.addEventListener('mousedown', userPicElementMouseDownHandler);
+uploadElement.addEventListener('mousedown', uploadElementMouseDownHandler);
