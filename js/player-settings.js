b7bd4feb0c648902eae6b4f3bf244e67e4bc1f51
ilg2005 <igoldfarb10@gmail.com>
2018-12-11 10:55:34 +0300

    исключено дублирование констант в модуле player-settings

--- js/player-settings.js
+++ js/player-settings.js
@@ -1,8 +1,6 @@
 'use strict';
 
 (function () {
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
   var setupWizardElement = window.utilities.setupElement.querySelector('.setup-wizard');
@@ -27,10 +25,10 @@
   };
 
   var setupWizardCoatClickHandler = function () {
-    changeWizardFeature('coat-color', COAT_COLORS, setupWizardCoatElement);
+    changeWizardFeature('coat-color', window.consts.COAT_COLORS, setupWizardCoatElement);
   };
   var setupWizardEyesClickHandler = function () {
-    changeWizardFeature('eyes-color', EYES_COLORS, setupWizardEyesElement);
+    changeWizardFeature('eyes-color', window.consts.EYES_COLORS, setupWizardEyesElement);
   };
   var setupWizardFireballClickHandler = function () {
     changeWizardFeature('fireball-color', FIREBALL_COLORS, setupWizardFireballElement);
