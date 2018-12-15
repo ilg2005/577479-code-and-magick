7b1d5cbcad9a92a62175cb4b61820ff2103fed65
ilg2005 <igoldfarb10@gmail.com>
2018-12-15 16:05:44 +0300

    добавлен window.backend.save

--- js/backend.js
+++ js/backend.js
@@ -5,29 +5,33 @@
   var SERVER_RESPONSE_OK = 200;
   var LOADING_TIMEOUT = 10000;
 
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.timeout = LOADING_TIMEOUT;
+
+  xhr.addEventListener('load', function () {
+    if (xhr.status === SERVER_RESPONSE_OK) {
+      onLoad(xhr.response); // в коллбэк-функцию передается ответ с данными сервера
+    } else {
+      onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText); // в коллбэк-функцию передается сообщение сервера об ошибке
+    }
+  });
+
+  xhr.addEventListener('error', function () {
+    onError('Произошла ошибка соединения');
+  });
+
+  xhr.addEventListener('timeout', function () {
+    onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
+  });
+
   window.backend = {
     load: function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.timeout = LOADING_TIMEOUT;
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === SERVER_RESPONSE_OK) {
-          onLoad(xhr.response); // в коллбэк-функцию передается ответ с данными сервера
-        } else {
-          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText); // в коллбэк-функцию передается сообщение сервера об ошибке
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
-      });
-
       xhr.open('GET', URL + '\/data');
+      xhr.send();
+    },
+    save: function (data, onLoad, onError) {
+      xhr.open('POST', URL);
       xhr.send();
     }
   };
