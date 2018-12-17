7baee93fbb0d2b7f857217baaf790ef9ea8ca0da
ilg2005 <igoldfarb10@gmail.com>
2018-12-17 13:01:26 +0300

    исправлен адрес сервера

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/cod-and-magick';
+  var URL = 'https://js.dump.academy/code-and-magick';
   var SERVER_RESPONSE_OK = 200;
   var LOADING_TIMEOUT = 10000;
 
