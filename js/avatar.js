2701bf1f963918421404eac6af2d7fe60b07cd02
ilg2005 <igoldfarb10@gmail.com>
2019-01-06 16:33:58 +0300

    аватарка пользователя заменяется при клике на input[type=file]

--- js/avatar.js
+++ js/avatar.js
@@ -1,5 +1,27 @@
 'use strict';
 
 (function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var fileChooser = document.querySelector('.upload input[type=file]');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        window.userPicElement.src = reader.result;
+        window.setupOpenElement.firstElementChild.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
 
 })();
